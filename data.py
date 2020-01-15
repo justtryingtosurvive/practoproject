018a57de5a3824ecf7693f8368f19514abc5355e
Akash Parvatikar <pwrmasterserver@gmail.com>
2020-01-15 15:35:26 +0530

    Adding the base

---
+++ data.py
@@ -0,0 +1,28 @@
+def Articles():
+	articles = [
+				{
+				'id':'1',
+				'title': 'Article one',
+				'body':'Lorem ipsum what ever bhal ',
+				'author':'Akash P',
+				'create_date':'12-01-2020'
+				},
+				{
+				'id':'2',
+				'title': 'Article two',
+				'body':'Lorem ipsum what ever bleh ',
+				'author':'John Doe',
+				'create_date':'11-01-2020'
+				},
+
+				{
+				'id':'2',
+				'title': 'Article Three',
+				'body':'Lorem ipsum whatever man fuck off ',
+				'author':'Mary Jane',
+				'create_date':'10-01-2020'
+				}
+
+				]
+	return articles
+	
